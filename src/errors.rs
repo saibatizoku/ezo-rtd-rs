4313f76d45eb60fec7bf5b8883b3ec252db9fd4e
Federico Mena Quintero <federico@gnome.org>
2017-07-13 14:46:03 -0500

    response.rs: Make the parse() functions take a &str, not a &[u8]
    We assume that the response strings have already been cleaned up and
    validated.
    
    Also, remove MalformedResponse from errors.rs - this is now in the
    ezo-common-rs crate.

--- src/errors.rs
+++ src/errors.rs
@@ -1,12 +1,7 @@
 use ezo_common;
+
 error_chain! {
     errors {
-        // The response is not nul-terminated, or it is not valid ASCII/UTF-8
-        MalformedResponse {
-            description ("malformed response")
-            display ("response is not a valid nul-terminated UTF-8 string")
-        }
-
         // The response could not be parsed
         ResponseParse {
             description ("could not parse response")
