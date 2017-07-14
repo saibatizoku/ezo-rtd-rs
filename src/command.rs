88e1c3966439276ffe4ad34d72d2bc2a0a8e447f
Joaquín R <globojorro@gmail.com>
2017-07-14 13:37:33 -0500

    Port tests into refactored Command scheme

--- src/command.rs
+++ src/command.rs
@@ -16,12 +16,262 @@ mod tests {
     use super::*;
 
     #[test]
-    fn builds_temperature_scale_command() {
-        unimplemented!();
+    fn build_command_uart_300() {
+        let cmd = Baud(BpsRate::Bps300);
+        assert_eq!(cmd.get_command_string(), "Baud,300");
+        assert_eq!(cmd.get_delay(), 0);
     }
 
     #[test]
-    fn builds_sleep_command() {
-        unimplemented!();
+    fn build_command_uart_1200() {
+        let cmd = Baud(BpsRate::Bps1200);
+        assert_eq!(cmd.get_command_string(), "Baud,1200");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_2400() {
+        let cmd = Baud(BpsRate::Bps2400);
+        assert_eq!(cmd.get_command_string(), "Baud,2400");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_9600() {
+        let cmd = Baud(BpsRate::Bps9600);
+        assert_eq!(cmd.get_command_string(), "Baud,9600");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_19200() {
+        let cmd = Baud(BpsRate::Bps19200);
+        assert_eq!(cmd.get_command_string(), "Baud,19200");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_38400() {
+        let cmd = Baud(BpsRate::Bps38400);
+        assert_eq!(cmd.get_command_string(), "Baud,38400");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_57600() {
+        let cmd = Baud(BpsRate::Bps57600);
+        assert_eq!(cmd.get_command_string(), "Baud,57600");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_uart_115200() {
+        let cmd = Baud(BpsRate::Bps115200);
+        assert_eq!(cmd.get_command_string(), "Baud,115200");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_calibration_temperature() {
+        let cmd = CalibrationTemperature(35.2459);
+        assert_eq!(cmd.get_command_string(), "Cal,35.25");
+        assert_eq!(cmd.get_delay(), 1000);
+    }
+
+    #[test]
+    fn build_command_calibration_clear() {
+        let cmd = CalibrationClear;
+        assert_eq!(cmd.get_command_string(), "Cal,clear");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_calibration_state() {
+        let cmd = CalibrationState;
+        assert_eq!(cmd.get_command_string(), "Cal,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_data_logger_period() {
+        let cmd = DataloggerPeriod(10);
+        assert_eq!(cmd.get_command_string(), "D,10");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_data_logger_disable() {
+        let cmd = DataloggerDisable;
+        assert_eq!(cmd.get_command_string(), "D,0");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_data_logger_interval() {
+        let cmd = DataloggerInterval;
+        assert_eq!(cmd.get_command_string(), "D,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_change_device_address() {
+        let cmd = DeviceAddress(88);
+        assert_eq!(cmd.get_command_string(), "I2C,88");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_device_information() {
+        let cmd = DeviceInformation;
+        assert_eq!(cmd.get_command_string(), "I");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_export() {
+        let cmd = Export;
+        assert_eq!(cmd.get_command_string(), "Export");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_export_info() {
+        let cmd = ExportInfo;
+        assert_eq!(cmd.get_command_string(), "Export,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_import() {
+        let calibration_string = "ABCDEFGHIJKLMNO".to_string();
+        let cmd = Import(calibration_string);
+        assert_eq!(cmd.get_command_string(), "Import,ABCDEFGHIJKLMNO");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_factory() {
+        let cmd = Factory;
+        assert_eq!(cmd.get_command_string(), "Factory");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_find() {
+        let cmd = Find;
+        assert_eq!(cmd.get_command_string(), "F");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_led_on() {
+        let cmd = LedOn;
+        assert_eq!(cmd.get_command_string(), "L,1");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_led_off() {
+        let cmd = LedOff;
+        assert_eq!(cmd.get_command_string(), "L,0");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_led_state() {
+        let cmd = LedState;
+        assert_eq!(cmd.get_command_string(), "L,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_memory_clear() {
+        let cmd = MemoryClear;
+        assert_eq!(cmd.get_command_string(), "M,clear");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_memory_recall() {
+        let cmd = MemoryRecall;
+        assert_eq!(cmd.get_command_string(), "M");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_memory_recall_location() {
+        let cmd = MemoryRecallLastLocation;
+        assert_eq!(cmd.get_command_string(), "M,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_plock_enable() {
+        let cmd = ProtocolLockEnable;
+        assert_eq!(cmd.get_command_string(), "Plock,1");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_plock_disable() {
+        let cmd = ProtocolLockDisable;
+        assert_eq!(cmd.get_command_string(), "Plock,0");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_plock_status() {
+        let cmd = ProtocolLockState;
+        assert_eq!(cmd.get_command_string(), "Plock,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_reading() {
+        let cmd = Reading;
+        assert_eq!(cmd.get_command_string(), "R");
+        assert_eq!(cmd.get_delay(), 600);
+    }
+
+    #[test]
+    fn build_command_scale_celsius() {
+        let cmd = ScaleCelsius;
+        assert_eq!(cmd.get_command_string(), "S,c");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_scale_kelvin() {
+        let cmd = ScaleKelvin;
+        assert_eq!(cmd.get_command_string(), "S,k");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_scale_fahrenheit() {
+        let cmd = ScaleFahrenheit;
+        assert_eq!(cmd.get_command_string(), "S,f");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_scale_status() {
+        let cmd = ScaleState;
+        assert_eq!(cmd.get_command_string(), "S,?");
+        assert_eq!(cmd.get_delay(), 300);
+    }
+
+    #[test]
+    fn build_command_sleep_mode() {
+        let cmd = Sleep;
+        assert_eq!(cmd.get_command_string(), "Sleep");
+        assert_eq!(cmd.get_delay(), 0);
+    }
+
+    #[test]
+    fn build_command_device_status() {
+        let cmd = Status;
+        assert_eq!(cmd.get_command_string(), "Status");
+        assert_eq!(cmd.get_delay(), 300);
     }
 }
