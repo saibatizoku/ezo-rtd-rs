a5e0e1fe95ab832ff6b7f9b0996272547c140458
Joaquín R <globojorro@gmail.com>
2017-07-16 17:58:53 -0500

    FIXME: examples compile but don't work; transition to new API

--- examples/read-loop.rs
+++ examples/read-loop.rs
@@ -5,12 +5,13 @@ extern crate chrono;
 extern crate ezo_rtd;
 extern crate i2cdev;
 
-use chrono::{DateTime, Utc};
-use ezo_rtd::errors::*;
-use ezo_rtd::{CommandBuilder, I2cCommand, TemperatureCommand};
-use i2cdev::linux::LinuxI2CDevice;
 use std::thread;
 use std::time::Duration;
+
+use chrono::{DateTime, Utc};
+use ezo_rtd::errors::*;
+use ezo_rtd::command::{Command, Reading, Sleep};
+use i2cdev::linux::LinuxI2CDevice;
 
 const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 101; // could be specified as 0x65
@@ -20,17 +21,22 @@ fn run() -> Result<()> {
     let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
         .chain_err(|| "Could not open I2C device")?;
     loop {
-        let mut builder = TemperatureCommand::Reading.build();
-        builder.run(&mut dev)?;
-        let temp = builder.parse_response()?;
-        TemperatureCommand::Sleep.build().run(&mut dev)?;
-        let dt: DateTime<Utc> = Utc::now();
-        println!("{:?},{:.*},°C",
-                 dt,
-                 2,
-                 temp.parse::<f64>().chain_err(|| "unparsable temperature")?);
+        let _ = Reading.run(&mut dev)?;
+        let _ = Sleep.run(&mut dev)?;
+        // let _ = _print_response(cmd.parse(), 2, "°C");
         thread::sleep(Duration::from_millis(9400));
     }
+}
+
+fn _print_response(response: &str, decimals: usize, units: &str) -> Result<()> {
+        let dt: DateTime<Utc> = Utc::now();
+        println!("{:?},{:.*},{}",
+                 dt,
+                 decimals,
+                 response.parse::<f64>().chain_err(|| "unparsable temperature")?,
+                 units,
+                );
+        Ok(())
 }
 
 fn main() {
--- examples/state-of-sensor.rs
+++ examples/state-of-sensor.rs
@@ -5,7 +5,16 @@ extern crate ezo_rtd;
 extern crate i2cdev;
 
 use ezo_rtd::errors::*;
-use ezo_rtd::{CommandBuilder, I2cCommand, TemperatureCommand};
+use ezo_rtd::command::{
+    Command,
+    Status,
+    CalibrationState,
+    DataloggerInterval,
+    LedState,
+    ExportInfo,
+    Export,
+    Sleep,
+};
 use i2cdev::linux::LinuxI2CDevice;
 
 const I2C_BUS_ID: u8 = 1;
@@ -15,24 +24,15 @@ fn run() -> Result<()> {
     let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
     let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
         .chain_err(|| "Could not open I2C device")?;
-    let mut response = String::new();
-    let commands = [TemperatureCommand::Status,
-                    TemperatureCommand::CalibrationState,
-                    TemperatureCommand::DataloggerInterval,
-                    TemperatureCommand::LedState,
-                    TemperatureCommand::ExportInfo,
-                    TemperatureCommand::Export,
-                    TemperatureCommand::Export,
-                    TemperatureCommand::Export,
-                    TemperatureCommand::Sleep];
-    for cmd in commands.iter() {
-        let mut builder = cmd.build();
-        builder.run(&mut dev)?;
-        response += &builder.parse_response()?;
-        response += &"\n";
-    }
-    println!("responses:");
-    println!("{}", response);
+    let _ = Status.run(&mut dev)?;
+    let _ = CalibrationState.run(&mut dev)?;
+    let _ = DataloggerInterval.run(&mut dev)?;
+    let _ = LedState.run(&mut dev)?;
+    let _ = ExportInfo.run(&mut dev)?;
+    let _ = Export.run(&mut dev)?;
+    let _ = Export.run(&mut dev)?;
+    let _ = Export.run(&mut dev)?;
+    let _ = Sleep.run(&mut dev)?;
     Ok(())
 }
 
