26aec908f062ef7b798462a0dc9d1a365e45e4b6
Joaquín R <globojorro@gmail.com>
2017-07-18 13:29:12 -0500

    errors when using define_macro!

--- src/errors.rs
+++ src/errors.rs
@@ -3,9 +3,19 @@ use ezo_common;
 
 error_chain! {
     errors {
+        // The unsuccessful response code
+        I2CRead {
+            description ("unsuccessful device read")
+            display ("response was not obtainable")
+        }
         // The response could not be parsed
         ResponseParse {
             description ("could not parse response")
+        }
+        // Any response code that is not `Success`
+        UnsuccessfulResponse {
+            description ("unsuccessful response code")
+            display ("response code was not successful")
         }
     }
     links {
