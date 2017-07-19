b0d4268b2cf123c4cdca3ba3f786f679b3acae96
Joaquín R <globojorro@gmail.com>
2017-07-19 00:10:30 -0500

    new error for define_command! macro

--- src/errors.rs
+++ src/errors.rs
@@ -8,6 +8,11 @@ error_chain! {
             description ("unsuccessful device read")
             display ("response was not obtainable")
         }
+        // The response is not nul-terminated, or it is not valid ASCII/UTF-8
+        MalformedResponse {
+            description ("malformed response")
+            display ("response is not a valid nul-terminated UTF-8 string")
+        }
         // The response could not be parsed
         ResponseParse {
             description ("could not parse response")
